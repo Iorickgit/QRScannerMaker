4f6090c217a77a38e5c1b650ff2dfe907ca5c43e
南　伊織 <iori.minami@gmail.com>
2020-10-03 21:03:22 +0900

    Initial Commit

---
+++ QRCodeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRCodeApp
+//
+//  Created by 南　伊織 on 2020/10/03.
+//  Copyright © 2020 南　伊織. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ QRCodeAppTests/QRCodeAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  QRCodeAppTests.swift
+//  QRCodeAppTests
+//
+//  Created by 南　伊織 on 2020/10/03.
+//  Copyright © 2020 南　伊織. All rights reserved.
+//
+
+import XCTest
+@testable import QRCodeApp
+
+class QRCodeAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
