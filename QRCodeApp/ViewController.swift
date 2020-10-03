eb01c0e23b906e8188f3f90ee79de1ec59a641b5
南　伊織 <iori.minami@gmail.com>
2020-10-03 21:37:24 +0900

    initial commit for QRCodes

--- QRCodeApp/ViewController.swift
+++ QRCodeApp/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        imageView.image = generateQRCode(from: "STRINGFROMID")
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func generateQRCode(from string: String) -> UIImage? {
+        let data = string.data(using: String.Encoding.ascii)
+        if let filter = CIFilter(name: "CIQRCodeGenerator") {
+            filter.setValue(data, forKey: "inputMessage")
+            let transform = CGAffineTransform(scaleX: 100, y: 100)
+            if let output = filter.outputImage?.transformed(by: transform) {
+                return UIImage(ciImage: output)
+            }
+        }
+        return nil
+    }
 }
-
